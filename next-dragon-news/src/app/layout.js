4ea691a3d6fbeb2d0644cadb15b1b10d7d9ef1fd
toqitahmid <rubaiathali@gmail.com>
2026-06-06 11:06:35 +0600

    intigrated user authentication vai better auth email & password

--- next-dragon-news/src/app/layout.js
+++ next-dragon-news/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme = 'light'
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Toaster position="top-center" />
+        {children}
+      </body>
     </html>
   );
 }
--- next-dragon-news/src/lib/auth.js
+++ next-dragon-news/src/lib/auth.js
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
